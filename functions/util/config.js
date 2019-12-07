71fda155ac9304a58558dabeeab619d640d41ff6
Kingkejriwal <Sarang.kejriwal@gmail.com>
2019-12-07 13:20:06 +0530

    network app functions

---
+++ functions/util/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    apiKey: "AIzaSyDRMxR0Hc7T3GvXbH7SICiDM2BGK4YjRiM",
+    authDomain: "networkape-60493.firebaseapp.com",
+    databaseURL: "https://networkape-60493.firebaseio.com",
+    projectId: "networkape-60493",
+    storageBucket: "networkape-60493.appspot.com",
+    messagingSenderId: "881927641468",
+    appId: "1:881927641468:web:cf42d648e6967446"
+  };
